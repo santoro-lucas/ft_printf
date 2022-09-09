3ebdfbd86c27ca94be38cd7d19cda8b0e581f3e7
lusantor <lusantor@student.42.rio>
2022-09-09 20:35:16 -0300

    Drafts conditionals for ft_printf

--- ft_printf.c
+++ ft_printf.c
@@ -1,9 +1,35 @@
 #include "libft/libft.a"
 #include <stdio.h>
+#include <stdarg.h>
+
+int	ft_putchar(char c)
+{
+	ft_putchar_fd(char c, 1);
+}
 
 int	ft_printf(const char *string, ...)
 {
+	va_list	argument;
+	int		offset;
 
+	offset = 0;
+	va_start(argument, string);
+	while (*(string + offset))
+	{
+		if (*(string + offset) == '%')
+		{
+			offset++;
+			if (*(string + offset) == '%')
+				ft_putchar('%');
+			if (*(string + offset) == 'c')
+				ft_putchar(va_arg(argument, char));
+			if (*(string + offset) == 's')
+				ft_putstr(va_arg(argument, char *));
+			if (ft_strchr("pdiuxX", *(string + offset))
+				putnbr_2(va_arg(argument, int));
+		}
+	}
+	va_end(argument);
 }
 
 int main(void)
