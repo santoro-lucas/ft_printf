bbd012782b5048eab93fd90cf034d562a04e05d1
lusantor <lusantor@student.42.rio>
2022-09-14 17:50:27 -0300

    function passes all tests and norminette

--- ft_printf.c
+++ ft_printf.c
@@ -11,7 +11,38 @@
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-                                                                                                                                                                                                                                                                                                   
+
+// int	ft_printf(const char *string, ...)
+// {
+// 	va_list	args;
+// 	int		counter;
+
+// 	counter = 0;
+// 	va_start(args, string);
+// 	while (*string)
+// 	{
+// 		if (*string == '%')
+// 		{
+// 			string++;
+// 			if (*string == '%')
+// 				counter += ft_putchar(*string);
+// 			if (*string == 'c')
+// 				counter += ft_putchar(va_arg(args, int));
+// 			if (*string == 's')
+// 				counter += ft_putstr(va_arg(args, char *));
+// 			if (ft_strchr("diuxX", *string))
+// 				counter += ft_parse_nbr(va_arg(args, int), *string);
+// 			if (*string == 'p')
+// 				counter += ft_parse_nbr(va_arg(args, unsigned long), *string);
+// 		}
+// 		else
+// 			counter += ft_putchar(*string);
+// 		string++;
+// 	}
+// 	va_end(args);
+// 	return (counter);
+// }
+
 int	ft_printf(const char *string, ...)
 {
 	va_list	args;
@@ -22,19 +53,17 @@ int	ft_printf(const char *string, ...)
 	while (*string)
 	{
 		if (*string == '%')
-		{
 			string++;
-			if (*string == '%')
-				counter += ft_putchar(*string);
-			if (*string == 'c')
-				counter += ft_putchar(va_arg(args, int));
-			if (*string == 's')
-				counter += ft_putstr(va_arg(args, char *));
-			if (ft_strchr("diuxX", *string))
-				counter += ft_parse_nbr(va_arg(args, int), *string);
-			if (*string == 'p')
-				counter += ft_parse_nbr(va_arg(args, unsigned long), *string);
-		}
+		if (*(string - 1) == '%' && *string == '%')
+			counter += ft_putchar(*string);
+		else if (*(string - 1) == '%' && *string == 'c')
+			counter += ft_putchar(va_arg(args, int));
+		else if (*(string - 1) == '%' && *string == 's')
+			counter += ft_putstr(va_arg(args, char *));
+		else if (*(string - 1) == '%' && ft_strchr("diuxX", *string))
+			counter += ft_parse_nbr(va_arg(args, int), *string);
+		else if (*(string - 1) == '%' && *string == 'p')
+			counter += ft_parse_nbr(va_arg(args, unsigned long), *string);
 		else
 			counter += ft_putchar(*string);
 		string++;
